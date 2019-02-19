344d17d71320bb4d13ef52ce0cecdc247358eab7
Jane Kim <kimjane7@msu.edu>
2019-02-19 03:00:12 -0500

    finished homework 4

--- QR_decomp.py
+++ QR_decomp.py
@@ -4,12 +4,12 @@ from scipy.linalg import eigvalsh, expm
 
 class QR_Decomposition:
 
-
 	def __init__(self, A):
 
 		self.m, self.n = A.shape
 		self.A = A
 		self.tolerance = 1E-30
+
 
 	def classical_GramSchmidt(self):
 
@@ -56,9 +56,10 @@ class QR_Decomposition:
 				V[:,j] = V[:,j]-self.R[i,j]*self.Q[:,i]
 
 
+
 	def Householder(self):
 
-		self.Q = np.zeros((self.m,self.n),dtype=complex)
+		self.Q = np.zeros((self.m,self.m),dtype=complex)
 		self.R = self.A.copy()
 		V = np.zeros((self.m,self.n),dtype=complex)
 
@@ -66,21 +67,29 @@ class QR_Decomposition:
 
 			# householder transformation
 			x = self.R[k:self.m,k]
+
+			# construct elementary vector
 			e1 = np.zeros(self.m-k)
 			e1[0] = 1.0
-			vk = np.sign(x[0])*np.linalg.norm(x)*e1+x
+
+			# get sign
+			sgn = np.sign(x[0])
+			if sgn == 0:
+				sgn = 1+0j	
+
+			# reflection vector
+			vk = sgn*np.linalg.norm(x)*e1+x
 			if np.linalg.norm(vk) > self.tolerance:
 				vk = vk/np.linalg.norm(vk)
 
 			# store to calculate Q
 			V[k:self.m,k] = vk
 
-			# calculate R		
-			self.R[k:self.m,k:self.n] = np.dot(np.eye(self.m-k)-2.0*np.outer(vk,np.conjugate(vk)),self.R[k:self.m,k:self.n])
-
+			# calculate R
+			self.R[k:self.m,k:self.n] = self.R[k:self.m,k:self.n]-2.0*np.outer(vk,np.dot(np.conjugate(vk),self.R[k:self.m,k:self.n]))
 
 		# calculate Q*I
-		for i in range(self.n):
+		for i in range(self.m):
 
 			ei = np.zeros(self.m,dtype=complex)
 			ei[i] = 1.0
@@ -93,47 +102,133 @@ class QR_Decomposition:
 			self.Q[:,i] = ei
 
 
-
-	def check_Q_orthogonal(self,tolerance):
+	def check_Q_orthogonal(self, tolerance):
 
 		np.set_printoptions(formatter={'complexfloat':lambda x:('{:>10.8f}{:>+10.8f}i'.format(x.real,x.imag) if (x.real != 0.0 and x.imag != 0.0) else ('    {:>10.8f}    '.format(x.real) if x.imag == 0.0 else'     {:>10.8f}i   '.format(x.imag)))})
 
 		QTQ = np.dot(np.transpose(self.Q),self.Q)
+		dim = np.size(QTQ,0)
 		print("\nQ^T Q = ",QTQ[0,:])
-		for i in range(1,self.n):
+		for i in range(1,dim):
 			print(" "*8,QTQ[i,:])
 		print("")
 
-		if np.allclose(QTQ,np.eye(self.n),rtol=0.0,atol=tolerance):
+		if np.allclose(QTQ,np.eye(dim),rtol=0.0,atol=tolerance):
 			print("Q is orthogonal (tolerance = ", tolerance, ")")
 		else:
 			print("Q is not orthogonal (tolerance = ", tolerance, ")")
 
 
-	def compare(self):
 
-		print("\nClassical Gram-Schmidt Method:")
-		self.classical_GramSchmidt()
-		self.display_result()
-		self.check_Q_orthogonal(1E-10)
 
-		print("\nModified Gram-Schmidt Method:")
-		self.modified_GramSchmidt()
-		self.display_result()
-		self.check_Q_orthogonal(1E-10)
+	# solve Ax=b for x, assuming A=QR has already been calculated
+	def solve(self, b):
 
-		print("\nHouseholder Transform Based Method:")
-		self.Householder()
-		self.display_result()
-		self.check_Q_orthogonal(1E-10)
+		# compute y=Q*b
+		y = np.dot(np.transpose(np.conjugate(self.Q)),b)
+
+		# solve Rx=y via back substitution
+		x = np.zeros(self.n,dtype=complex)
+		for i in range(self.n-1,-1,-1):
+			x[i] = y[i]
+			for j in range(i+1,self.n):
+				x[i] += -self.R[i,j]*x[j]
+			if(abs(self.R[i,i])>self.tolerance):
+				x[i] = x[i]/self.R[i,i]
+
+		print("\n","x = ", x[0])
+		for i in range(1,np.size(x)):
+			print(" "*5,x[i])
+
+		print("\n","||Ax-b|| = ",np.linalg.norm(np.dot(self.A,x)-b))
+
+		return x,np.linalg.norm(np.dot(self.A,x)-b)
+
+		'''
+		# display result
+		names = ["b","x","Ax-b"]
+		vectors = [b,x,np.dot(self.A,x)-b]
+		dims = [self.m,self.n,self.m]
+		widths = [6,6,9]
+
+		np.set_printoptions(formatter={'complexfloat':lambda x:('{:>10.8f}{:>+10.8f}i'.format(x.real,x.imag) if (x.real != 0.0 and x.imag != 0.0) else ('    {:>10.8f}    '.format(x.real) if x.imag == 0.0 else'     {:>10.8f}i   '.format(x.imag)))})
+
+		for n in range(len(names)):
+			print("\n", names[n]," = ",vectors[n][0])
+			for i in range(1,dims[n]):
+				print(" "*widths[n],vectors[n][i])
+		'''
+
+
+
+	def compare(self, b):
+
+		# over-determined systems
+		if self.m >= self.n:
+
+			print("\nClassical Gram-Schmidt Method:")
+			self.classical_GramSchmidt()
+			self.display_result()
+			self.check_Q_orthogonal(1E-10)
+			self.solve(b)
+
+			print("\nModified Gram-Schmidt Method:")
+			self.modified_GramSchmidt()
+			self.display_result()
+			self.check_Q_orthogonal(1E-10)
+			self.solve(b)
+
+			print("\nHouseholder Transform Based Method:")
+			self.Householder()
+			self.display_result()
+			self.check_Q_orthogonal(1E-10)
+			self.solve(b)
+
+		# under-determined systems
+		else:
+
+			# QR factorize A* instead of A
+			self.A = np.transpose(np.conjugate(self.A))
+			self.m, self.n = self.A.shape
+
+			# full QR factorization
+			print("\nHouseholder Transform Based Method:")
+			self.Householder()
+
+			# extract R*
+			R_star = np.transpose(np.conjugate(self.R[:self.n,:self.n]))
+			
+			# forward substitution to solve R*y_R = b
+			y = np.zeros(self.m,dtype=complex)
+			for i in range(self.n):
+				y[i] = b[i]
+				for j in range(i):
+					y[i] += -R_star[i,j]*y[j]
+				if (abs(R_star[i,i])>self.tolerance):
+					y[i] = y[i]/R_star[i,i]
+
+			# solve for x
+			x = np.dot(self.Q,y)
+			
+			# display
+			self.display_result()
+			self.check_Q_orthogonal(1E-10)
+
+			np.set_printoptions(formatter={'complexfloat':lambda x:('{:>10.8f}{:>+10.8f}i'.format(x.real,x.imag) if (x.real != 0.0 and x.imag != 0.0) else ('    {:>10.8f}    '.format(x.real) if x.imag == 0.0 else'     {:>10.8f}i   '.format(x.imag)))})
+
+			print("\n","x = ", x[0])
+			for i in range(1,np.size(x)):
+				print(" "*5,x[i])
+
+			print("\n","||Ax-b|| = ",np.linalg.norm(np.dot(np.transpose(np.conjugate(self.A)),x)-b))
 
 
 
 	def display_result(self):
 
 		names = ["A","Q","R","A-QR"]
-		matrices = [self.A,self.Q,self.R,self.A-np.dot(self.Q,self.R[:self.n,:self.n])]
-		dims = [self.m,self.m,self.n,self.m]
+		matrices = [self.A,self.Q,self.R,self.A-np.dot(self.Q,self.R)]
+		dims = [np.size(matrices[n],0) for n in range(4)]
 		widths = [6,6,6,9]
 
 		np.set_printoptions(formatter={'complexfloat':lambda x:('{:>10.8f}{:>+10.8f}i'.format(x.real,x.imag) if (x.real != 0.0 and x.imag != 0.0) else ('    {:>10.8f}    '.format(x.real) if x.imag == 0.0 else'     {:>10.8f}i   '.format(x.imag)))})
@@ -143,3 +238,9 @@ class QR_Decomposition:
 			for i in range(1,dims[n]):
 				print(" "*widths[n],matrices[n][i,:])
 
+
+
+
+
+
+				
--- main.py
+++ main.py
@@ -1,14 +1,63 @@
 from QR_decomp import QR_Decomposition
 import numpy as np 
+from scipy.linalg import hilbert
+
+# Tikhonov regularization
+def Hilbert_test(order_list, alpha_list):
+
+	print("{:<6s}{:^10s}{:^18s}{:^18s}{:^18s}{:^18s}{:^18s}{:^18s}\n".format("n","alpha","||Ax-b|| (cGS)","||x|| (cGS)","||Ax-b|| (mGS)","||x|| (mGS)","||Ax-b|| (H)","||x|| (H)"))
+
+	for n in order_list:
+
+		b_reg = np.zeros(2*n,dtype=complex)
+		for i in range(n):
+			b_reg[i] = 1.0; 
+
+		H = hilbert(n)
+		H_reg = np.zeros((2*n,n),dtype=complex)
+		H_reg[:n,:n] = H
+
+		for alpha in alpha_list:
+
+			H_reg[n:,:n] = alpha*np.eye(n,dtype=complex)
+
+			QR_H_reg = QR_Decomposition(H_reg)
+
+			QR_H_reg.classical_GramSchmidt()
+			x1, norm1 = QR_H_reg.solve(b_reg)
+
+			QR_H_reg.modified_GramSchmidt()
+			x2, norm2 = QR_H_reg.solve(b_reg)
+
+			QR_H_reg.Householder()
+			x3, norm3 = QR_H_reg.solve(b_reg)
+
+			print("{:<6d}{:^10.1e}{:^18.8f}{:^18.8f}{:^18.8f}{:^18.8f}{:^18.8f}{:^18.8f}".format(n,alpha,norm1,np.linalg.norm(x1),norm2,np.linalg.norm(x2),norm3,np.linalg.norm(x3)))
+
+		print(" ")
+				
 
 def main():
 
-	Z = np.array([[1,2,3],[4,5,6],[7,8,7],[4,2,3],[4,2,2]],dtype=complex)
-	QR_Z = QR_Decomposition(Z)
-	QR_Z.compare()
+	#Hilbert_test([10,20,30],[1.0E-8,1.0E-5,1.0E-3,0.1])
 
+	
+	# 2 (a)
+	A = np.array([[1,2,3],[4,5,6],[7,8,7],[4,2,3],[4,2,2]],dtype=complex)
+	QR_A = QR_Decomposition(A)
+	QR_A.compare(np.array([1,1,1,1,1],dtype=complex))
+	
+	# 2 (b)
 	A = np.array([[0.70000,0.70711],[0.70001,0.70711]],dtype=complex)
 	QR_A = QR_Decomposition(A)
-	QR_A.compare()
+	QR_A.compare(np.array([1,1],dtype=complex))
+
+	# 2 (c)
+	A = np.array([[1,2,3],[4,2,9]],dtype=complex)
+	QR_A = QR_Decomposition(A)
+	QR_A.compare(np.array([6,15],dtype=complex))
+	
+
+
 
 main()
